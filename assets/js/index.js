0dd24fc21cb47d5e6c03cb93214a03a5f5c5ff76
nandemotoken <nandemotoken@gmail.com>
2021-07-08 12:33:29 +0000

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+window.onload = async function() {
+//     contractaddress = "0x2a46f2ffd99e19a89476e2f62270e0a35bbf0756";
+
+//     provider = await new ethers.ethers.providers.EtherscanProvider( network = "homestead");
+
+//     nftcontract = await new ethers.Contract( contractaddress , abi , provider );
+
+//     balance = await nftcontract.ownerOf(40913);
+//     console.log("NFTの所有者は"+balance+"です。");
+
+//     tokenURI = await nftcontract.tokenURI(40913);
+//     console.log("NFTのメタデータは"+tokenURI+"です。");
+    
+}
+
